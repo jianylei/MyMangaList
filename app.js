06def8261c0c47a9b9cdc18f348240f7a0fbbb42
jianylei <jianyonglei@gmail.com>
2021-08-30 21:43:45 -0400

    added Book constructor

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-class Book{
-    constructor(title, author, ){
+let myLibrary = [{
+    title: "batman",
+    author: "guy who wrote batman",
+    isbn: "6654",
+    completed: true
+},{
+    title: "spiderman",
+    author: "guy who wrote spiderman",
+    isbn: "5154",
+    completed: false
+}];
 
+class Book{
+    constructor(title="", author="", isbn="", completed = false){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.completed = completed;
     }
 }
+
+
