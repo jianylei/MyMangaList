3a42bceddfd30dedb894234fdda83954db958b56
jianylei <jianyonglei@gmail.com>
2021-08-30 22:10:20 -0400

    completed Library.display(), and Library.addToList(x) functions

--- app.js
+++ app.js
@@ -19,4 +19,33 @@ class Book{
     }
 }
 
+class Library {
+    static display() {
+        const books = myLibrary;
+        if (books[0]) {
+            bookView.hidden = false;
+            books.forEach((book) => Library.addToList(book))
+        }
+    }
+    static addToList(book) {
+        let row = document.createElement("tr");
+        let checked = (book.completed) ? "checked" : "";
+        row.innerHTML = `
+            <td> ${book.title} </td>
+            <td> ${book.author} </td>
+            <td> ${book.isbn} </td>
+            <td> <div class="form-switch main-switch mt-1 mb-4">
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" ${checked}>
+            </div> </td>
+            <td><a href="" class="btn btn-danger btn-sm delete"> tmp </a></td>
+        `;
+        bookList.appendChild(row);
+    }
+}
 
+const bookView = document.querySelector(".table-container");
+const bookList = document.querySelector("#book-list");
+//events
+document.addEventListener("DOMContentLoaded", function(){
+    Library.display();
+});
