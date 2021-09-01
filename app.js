192c80949e55c42b214e251aeab829836fb1f268
jianylei <jianyonglei@gmail.com>
2021-08-31 21:45:39 -0400

    fixed form submit lisenter in accordance to layou changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let completedflag = false;
+let mangaflag = false;
 
 let myLibrary = [{
     title: "Kimetsu No Yaiba",
     author: "Koyoharu Gotouge",
+    manga: true,
     completed: true,
     pages: 205,
     read: 205,
@@ -10,13 +12,23 @@ let myLibrary = [{
 },{
     title: "Jujutsu Kaisen",
     author: "Gege Akutami",
+    manga: true,
     completed: false,
     pages: 156,
     read: 148,
     url: "https://static.wikia.nocookie.net/jujutsu-kaisen/images/3/31/Volume_4.png/revision/latest/scale-to-width-down/764?cb=20190226191850"
 },{
+    title: "The Outsiders",
+    author: "S. E. Hinton",
+    manga: false,
+    completed: false,
+    pages: 192,
+    read: 145,
+    url: "https://inyrmargins.files.wordpress.com/2011/12/outsiders-17.jpg"
+},{
     title: "Bleach",
     author: "Tite Kubo",
+    manga: true,
     completed: true,
     pages: 686,
     read: 686,
@@ -24,6 +36,7 @@ let myLibrary = [{
 },{
     title: "Kaguya-sama: Love Is War",
     author: "Aka Akasaka",
+    manga: true,
     completed: false,
     pages: 233,
     read: 146,
@@ -31,6 +44,7 @@ let myLibrary = [{
 },{
     title: "Chainsaw Man",
     author: "Tatsuki Fujimoto",
+    manga: true,
     completed: false,
     pages: 97,
     read: 66,
@@ -38,9 +52,10 @@ let myLibrary = [{
 }];
 
 class Book{
-    constructor(title, author, completed, pages, read=0, url=""){
+    constructor(title, author, manga, completed, pages, read, url){
         this.title = title;
         this.author = author;
+        this.manga = manga;
         this.completed = completed;
         this.pages = pages;
         this.read = read;  
@@ -107,13 +122,24 @@ class Library {
         iClass.classList.add("far", "fa-check-circle");
         cardUpdateComplete.appendChild(iClass);
 
-        if(completed){
-            cardPages.innerText = `${book.pages} / ${book.pages} Pages`;
-            cardUpdateComplete.style.color = "var(--completed-color)"
+        if(book.manga){
+            if(completed){
+                cardPages.innerText = `${book.pages} / ${book.pages} Chapters`;
+                cardUpdateComplete.style.color = "var(--completed-color)"
+            }
+            else{
+                cardPages.innerText = `${book.read} / ${book.pages} Chapters`;
+            }
+        }else{
+            if(completed){
+                cardPages.innerText = `${book.pages} / ${book.pages} Pages`;
+                cardUpdateComplete.classList.add("isCompleted");
+            }
+            else{
+                cardPages.innerText = `${book.read} / ${book.pages} Pages`;
+            }
         }
-        else{
-            cardPages.innerText = `${book.read} / ${book.pages} Pages`;
-        }
+        
         cardBtn.appendChild(cardPages);
         cardBtn.appendChild(cardUpdatePage);
         cardBtn.appendChild(cardUpdateDelete);
@@ -124,24 +150,52 @@ class Library {
 
         //reset completed flag (form slider)
         completedflag = false;
+        mangaflag = false;
     }
     static clearForm(){
         document.querySelector("#title").value = "";
         document.querySelector("#author").value = "";
         document.querySelector("#isbn").value = "";
         document.querySelector(".check-slider").checked = false;
+        document.querySelector(".manga-slider").checked = false;
+        completedflag = false;
+        mangaflag = false;
     }
 }
 
 const cardView = document.querySelector(".card-grid");
 const formLink = document.querySelector("#form-link");
 const form = document.querySelector("#book-form");
-const formToggle = document.querySelector("#flexSwitchCheckDefault");
 const pagesRead = document.querySelector("#pages-read");
+
+//form selectors
+const titleForm = document.querySelector("#title");
+const authorForm = document.querySelector("#author");
+const mangaForm = document.querySelector(".manga-slider");
+const completedForm = document.querySelector(".check-slider");
+const pagesForm = document.querySelector("#pages-number");
+const readForm = document.querySelector("#pages-read");
+const coverURLForm = document.querySelector("#url");
+
+const pagesLabelForm = document.querySelector("#page-label");
+const readLabelForm = document.querySelector("#read-label");
+
 //events
 document.addEventListener("DOMContentLoaded", Library.display);
-
-formToggle.onclick = () => {
+//disable "read" is completed == true
+mangaForm.onclick = () => {
+    mangaflag = (mangaflag)? false: true;
+    
+    if(mangaflag) {
+        pagesLabelForm.innerText = "Number of Chapters"
+        readLabelForm.innerText = "Current Chapter Number"
+    }
+    else{
+        pagesLabelForm.innerText = "Number of Pages"
+        readLabelForm.innerText = "Current Pages Number"
+    }
+}
+completedForm.onclick = () => {
     completedflag = (completedflag)? false: true;
     
     if(completedflag) {
@@ -154,43 +208,45 @@ formToggle.onclick = () => {
     }
 }
 
-
-
-
 form.addEventListener("submit", function (e) {
     //prevent actual submission
     e.preventDefault();
+    let readPages;
 
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const isbn = document.querySelector("#isbn").value;
-    const completed = document.querySelector(".check-slider").checked;
-    
-    if(title && author){
-        let isInLibrary = myLibrary.some((book) => {
-            if(isbn === book.isbn){
-                validationFeedback.innerText = `ISBN#: ${isbn} (${title} by ${author}) has already been added!`
-                titleValidation.classList.add("is-invalid");
+    if(titleForm.value){
+        titleForm.classList.remove("is-invalid");
+
+        if(authorForm.value) {
+            authorForm.classList.remove("is-invalid");
+
+            if(pagesForm.value){
+                pagesForm.classList.remove("is-invalid");
                 
-                return true;
-            } 
-        }) 
-        
-        if(isInLibrary) {
-            
-        }
-        else{
-            const newBook = new Book(title, author, isbn, completed);
-            myLibrary.push(newBook);
-            Library.addToList(newBook);
-            titleValidation.classList.remove("is-invalid");
-            Library.clearForm();
+                if(completedForm.checked){
+                    readPages = pagesForm.value;
+                }
+                else if(readForm.value){
+                    readPages = readForm.value;
+                }else{
+                    readPages = 0;
+                }
+                
+            }else{
+                pagesForm.classList.add("is-invalid");
+            }
+        }else {
+            authorForm.classList.add("is-invalid");
         }
 
-        
+    }else{
+        titleForm.classList.add("is-invalid");
     }
-    
 
-    completedflag = false;
+    if(readPages){
+        let mangaCheck = (mangaForm.checked) ? true: false;
+        const newBook = new Book(titleForm.value, authorForm.value, mangaCheck, completedForm.checked, pagesForm.value, readPages, coverURLForm.value);
+        Library.addToList(newBook);
+        Library.clearForm();
+    }
 });
 
